c4b4f6b5180ae800a18bdb9b2f2622d1feb4c04a
Chris Winberry <chris@winberry.net>
2010-04-07 13:54:33 -0400

    Added automated tests in preparation for chunk parsing

---
+++ tests/12-text_only.js
@@ -0,0 +1,9 @@
+exports.name = "Only text";
+exports.html = "this is the text";
+exports.expected = [
+    {
+        raw: "this is the text",
+        data: "this is the text",
+        type: "text"
+    }
+];
