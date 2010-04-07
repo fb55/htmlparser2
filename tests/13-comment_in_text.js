d1ba5b77b24d32c17744df166311298d0beec99f
Chris Winberry <chris@winberry.net>
2010-04-07 18:22:49 -0400

    Added some more tests

---
+++ tests/13-comment_in_text.js
@@ -0,0 +1,19 @@
+exports.name = "Comment within text";
+exports.html = "this is <!-- the comment --> the text";
+exports.expected = [
+    {
+        raw: "this is ",
+        data: "this is ",
+        type: "text"
+    },
+    {
+        raw: " the comment ",
+        data: " the comment ",
+        type: "comment"
+    },
+    {
+        raw: " the text",
+        data: " the text",
+        type: "text"
+    }
+];
