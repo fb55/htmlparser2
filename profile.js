7fa46b9579dd9daf6cce726730409c29fc0301ad
Chris Winberry <chris@winberry.net>
2010-04-16 12:12:44 -0400

    Updated profile.js to load a live html page

--- profile.js
+++ profile.js
@@ -19,23 +19,34 @@ function TimeCode (loops, func) {
 	return(GetTime() - start);
 }
 
-var html = fs.readFileSync("testdata/trackerchecker.html");
-
-var timeNodeHtmlParser = TimeCode(100, function () {
-	var handler = new htmlparser.DefaultHandler(function(err, dom) {
-		if (err)
-			sys.debug("Error: " + err);
-		//	else
-		//		sys.debug(sys.inspect(dom, false, null));
-	});
-	var parser = new htmlparser.Parser(handler);
-	parser.ParseComplete(html);
-})
-
-var timeLibXmlJs = TimeCode(100, function () {
-	var dom = libxml.parseHtmlString(html);
-})
-
-sys.debug("NodeHtmlParser: "  + timeNodeHtmlParser);
-sys.debug("LibXmlJs: "  + timeLibXmlJs);
-sys.debug("Difference: " + ((timeNodeHtmlParser - timeLibXmlJs) / timeLibXmlJs) * 100);
+var html = "";
+http.createClient(80, "en.wikipedia.org")
+	.request("GET", "/wiki/Javascript", {host: "en.wikipedia.org"})
+	.addListener("response", function (response) {
+		if (response.statusCode == "200") {
+			response.setEncoding("utf8");
+			response.addListener("data", function (chunk) {
+				html += chunk;
+			}).addListener("end", function() {
+				var timeNodeHtmlParser = TimeCode(100, function () {
+					var handler = new htmlparser.DefaultHandler(function(err, dom) {
+						if (err)
+							sys.debug("Error: " + err);
+					});
+					var parser = new htmlparser.Parser(handler);
+					parser.ParseComplete(html);
+				})
+				
+				var timeLibXmlJs = TimeCode(100, function () {
+					var dom = libxml.parseHtmlString(html);
+				})
+				
+				sys.debug("NodeHtmlParser: "  + timeNodeHtmlParser);
+				sys.debug("LibXmlJs: "  + timeLibXmlJs);
+				sys.debug("Difference: " + ((timeNodeHtmlParser - timeLibXmlJs) / timeLibXmlJs) * 100);
+			});
+		}
+		else
+			sys.debug("Error: got response status " + response.statusCode);
+	})
+	.end();
