60f8d24fdff4a002e13323f61d21ff10739d363a
Felix Gnass <felix.gnass@neteye.de>
2010-11-18 19:04:58 +0100

    Fixed position calculation in chunked mode

--- tests/22-position_data.js
+++ tests/22-position_data.js
@@ -30,74 +30,71 @@ exports.options = {
 	  handler: {}
 	, parser: { includeLocation: true }
 };
-//TODO: re-instate test when chunked position tracking is fixed
-exports.html = "<FIXME>";
-exports.expected = [ { raw: 'FIXME', data: 'FIXME', type: 'tag', name: 'FIXME' } ];
-//exports.html = "<html>\r\n\n\t<title>The Title</title><body>\nHello world\r\n\n</body>\n\n</html>";
-//exports.expected = [
-//	{
-//		raw: 'html',
-//		data: 'html',
-//		type: 'tag',
-//		location: {
-//			line: 1,
-//			col: 1
-//		},
-//		name: 'html',
-//		children: [{
-//			raw: '\n\n',
-//			data: '\n\n',
-//			type: 'text',
-//			location: {
-//				line: 1,
-//				col: 5
-//			}
-//		}, {
-//			raw: 'title',
-//			data: 'title',
-//			type: 'tag',
-//			location: {
-//				line: 3,
-//				col: 2
-//			},
-//			name: 'title',
-//			children: [{
-//				raw: 'The Title',
-//				data: 'The Title',
-//				type: 'text',
-//				location: {
-//					line: 3,
-//					col: 6
-//				}
-//			}]
-//		}, {
-//			raw: 'body',
-//			data: 'body',
-//			type: 'tag',
-//			location: {
-//				line: 3,
-//				col: 1
-//			},
-//			name: 'body',
-//			children: [{
-//				raw: '\nHello world\n\n',
-//				data: '\nHello world\n\n',
-//				type: 'text',
-//				location: {
-//					line: 3,
-//					col: 5
-//				}
-//			}]
-//		}, {
-//			raw: '\n\n',
-//			data: '\n\n',
-//			type: 'text',
-//			location: {
-//				line: 6,
-//				col: 6
-//			}
-//		}]
-//	}
-//	];
+exports.html = "<html>\r\n\n\t<title>The Title</title><body>\nHello world\r\n\n</body>\n\n</html>";
+exports.expected = [
+	{
+		raw: 'html',
+		data: 'html',
+		type: 'tag',
+		name: 'html',
+		location: {
+			line: 1,
+			col: 1
+		},
+		children: [{
+			raw: '\r\n\n\t',
+			data: '\r\n\n\t',
+			type: 'text',
+			location: {
+				line: 1,
+				col: 7
+			}
+		}, {
+			raw: 'title',
+			data: 'title',
+			type: 'tag',
+			name: 'title',
+			location: {
+				line: 3,
+				col: 2
+			},
+			children: [{
+				raw: 'The Title',
+				data: 'The Title',
+				type: 'text',
+				location: {
+					line: 3,
+					col: 9
+				}
+			}]
+		}, {
+			raw: 'body',
+			data: 'body',
+			type: 'tag',
+			name: 'body',
+			location: {
+				line: 3,
+				col: 26
+			},
+			children: [{
+				raw: '\nHello world\r\n\n',
+				data: '\nHello world\r\n\n',
+				type: 'text',
+				location: {
+					line: 3,
+					col: 32
+				}
+			}]
+		}, {
+			raw: '\n\n',
+			data: '\n\n',
+			type: 'text',
+			location: {
+				line: 6,
+				col: 8
+			}
+		}]
+	}
+	];
 
 })();
