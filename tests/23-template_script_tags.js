dc5fe9c9ae212a09ea31948499d8c31672042191
Felix Böhm <me@feedic.com>
2011-10-25 22:47:06 +0200

    Added a test
    https://github.com/tautologistics/node-htmlparser/issues/29

---
+++ tests/23-template_script_tags.js
@@ -0,0 +1,15 @@
+exports.name = "Template script tags";
+exports.options = {
+	  handler: {}
+	, parser: {}
+};
+exports.html = "<script type=\"text/template\"> <h1>Heading1</h1></script>";
+exports.expected = [ { raw: 'script type="text/template"',
+    data: 'script type="text/template"',
+    type: 'script',
+    name: 'script',
+    attribs: { type: 'text/template' },
+    children: 
+     [ { raw: '<h1>Heading1</h1>',
+         data: '<h1>Heading1</h1>',
+         type: 'text' } ] } ];
