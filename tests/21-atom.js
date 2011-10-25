79fa74619865203ac62984b201d40a26ec11355d
Felix Böhm <me@feedic.com>
2011-10-25 22:46:16 +0200

    Deleted browser related content in tests

--- tests/21-atom.js
+++ tests/21-atom.js
@@ -1,33 +1,12 @@
-(function() {
-    function RunningInNode() {
-        return (
-            typeof require == "function" &&
-            typeof exports == "object" &&
-            typeof module == "object" &&
-            typeof __filename == "string" &&
-            typeof __dirname == "string"
-        );
-    }
-
-    if (!RunningInNode()) {
-        if (!this.Tautologistics) this.Tautologistics = {};
-        if (!this.Tautologistics.NodeHtmlParser)
-            this.Tautologistics.NodeHtmlParser = {};
-        if (!this.Tautologistics.NodeHtmlParser.Tests)
-            this.Tautologistics.NodeHtmlParser.Tests = [];
-        exports = {};
-        this.Tautologistics.NodeHtmlParser.Tests.push(exports);
-    }
-
-    exports.name = "Atom (1.0)";
-    exports.options = {
-        handler: {},
-        parser: {}
-    };
-    exports.type = "rss";
-    //http://en.wikipedia.org/wiki/Atom_%28standard%29
-    exports.html =
-        '<?xml version="1.0" encoding="utf-8"?>\
+exports.name = "Atom (1.0)";
+exports.options = {
+    handler: {},
+    parser: {}
+};
+exports.type = "rss";
+//http://en.wikipedia.org/wiki/Atom_%28standard%29
+exports.html =
+    '<?xml version="1.0" encoding="utf-8"?>\
 \
 <feed xmlns="http://www.w3.org/2005/Atom">\
 \
@@ -53,22 +32,21 @@
 	</entry>\
 \
 </feed>';
-    exports.expected = {
-        type: "atom",
-        id: "urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6",
-        title: "Example Feed",
-        link: "http://example.org/feed/",
-        description: "A subtitle.",
-        updated: new Date("2003-12-13T18:30:02Z"),
-        author: "johndoe@example.com",
-        items: [
-            {
-                id: "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a",
-                title: "Atom-Powered Robots Run Amok",
-                link: "http://example.org/2003/12/13/atom03",
-                description: "Some text.",
-                pubDate: new Date("2003-12-13T18:30:02Z")
-            }
-        ]
-    };
-})();
+exports.expected = {
+    type: "atom",
+    id: "urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6",
+    title: "Example Feed",
+    link: "http://example.org/feed/",
+    description: "A subtitle.",
+    updated: new Date("2003-12-13T18:30:02Z"),
+    author: "johndoe@example.com",
+    items: [
+        {
+            id: "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a",
+            title: "Atom-Powered Robots Run Amok",
+            link: "http://example.org/2003/12/13/atom03",
+            description: "Some text.",
+            pubDate: new Date("2003-12-13T18:30:02Z")
+        }
+    ]
+};
