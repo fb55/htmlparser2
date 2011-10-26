b51764486672bf81ca8ec766ab4405c33aea0bbb
Felix Böhm <me@feedic.com>
2011-10-26 15:43:51 +0200

    Added doctype to first test

--- tests/01-basic.js
+++ tests/01-basic.js
@@ -3,30 +3,26 @@ exports.options = {
 	  handler: {}
 	, parser: {}
 };
-exports.html = "<html><title>The Title</title><body>Hello world</body></html>";
-exports.expected =
-	[ { raw: 'html'
-		  , data: 'html'
-		  , type: 'tag'
-		  , name: 'html'
-		  , children: 
-		     [ { raw: 'title'
-		       , data: 'title'
-		       , type: 'tag'
-		       , name: 'title'
-		       , children: [ { raw: 'The Title', data: 'The Title', type: 'text' } ]
-		       }
-		     , { raw: 'body'
-		       , data: 'body'
-		       , type: 'tag'
-		       , name: 'body'
-		       , children: 
-		          [ { raw: 'Hello world'
-		            , data: 'Hello world'
-		            , type: 'text'
-		            }
-		          ]
-		       }
-		     ]
-		  }
-		];
+exports.html = "<!DOCTYPE html><html><title>The Title</title><body>Hello world</body></html>";
+exports.expected = [ { raw: '!DOCTYPE html',
+    data: '!DOCTYPE html',
+    type: 'directive',
+    name: '!DOCTYPE' },
+  { raw: 'html',
+    data: 'html',
+    type: 'tag',
+    name: 'html',
+    children: 
+     [ { raw: 'title',
+         data: 'title',
+         type: 'tag',
+         name: 'title',
+         children: [ { raw: 'The Title', data: 'The Title', type: 'text' } ] },
+       { raw: 'body',
+         data: 'body',
+         type: 'tag',
+         name: 'body',
+         children: 
+          [ { raw: 'Hello world',
+              data: 'Hello world',
+              type: 'text' } ] } ] } ];
