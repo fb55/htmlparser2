bc12cd8717f4ed1270538d39e2428c3d4f25d5fe
Felix Böhm <me@feedic.com>
2011-11-05 12:42:32 +0100

    Replaced _tagStack with _contentFlags, tweaked DefaultHandler
    That fixed https://github.com/tautologistics/node-htmlparser/issues/29.

--- tests/23-template_script_tags.js
+++ tests/23-template_script_tags.js
@@ -3,13 +3,13 @@ exports.options = {
 	  handler: {}
 	, parser: {}
 };
-exports.html = "<script type=\"text/template\"> <h1>Heading1</h1></script>";
+exports.html = "<script type=\"text/template\"><h1>Heading1</h1></script>";
 exports.expected = [ { raw: 'script type="text/template"',
     data: 'script type="text/template"',
     type: 'script',
     name: 'script',
     attribs: { type: 'text/template' },
     children: 
-     [ { raw: ' <h1>Heading1</h1>',
-         data: ' <h1>Heading1</h1>',
+     [ { raw: '<h1>Heading1</h1>',
+         data: '<h1>Heading1</h1>',
          type: 'text' } ] } ];
