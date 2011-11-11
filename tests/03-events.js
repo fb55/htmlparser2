c6fad57bd025fcf7d643a7d899500e02ebffa416
Felix Böhm <me@feedic.com>
2011-11-11 19:44:34 +0100

    Moved default callbacks for event tests to the processing part

--- tests/03-events.js
+++ tests/03-events.js
@@ -6,13 +6,25 @@ exports.dir = "./Events/";
 exports.test = function(test, cb){
 	var tokens = [];
 	var cbs = {
+		onopentag: function(name, attributes){
+			tokens.push({event:"open", name: name, attributes: attributes});
+		},
+		onclosetag: function(name){
+			tokens.push({event:"close", name: name});
+		},
+		ontext: function(text){
+			tokens.push({event:"text", text: text});
+		},
+		oncomment: function(data){
+			tokens.push({event:"comment", data:data});
+		},
+		onprocessinginstruction: function(name, data){
+			tokens.push({event:"processing", name:name, data:data});
+		},
 		onend: function(){
 			//deletes all tokens
 			cb(null, tokens.splice(0));
 		}
-	};
-	for(var i = 0; i < test.callbacks.length; i+=2){
-		cbs[test.callbacks[i]] = test.callbacks[i+1].bind(tokens);
 	};
 	var handler = new EventedHandler(cbs, test.options.handler);
 	helper.writeToParser(handler, test.options.parser, test.html);
--- tests/Events/01-simple.js
+++ tests/Events/01-simple.js
@@ -1,17 +1,6 @@
-exports.name = "Events";
+exports.name = "simple";
 exports.type = "event";
 exports.options = {handler: {}, parser: {}};
-exports.callbacks = [
-	"onopentag", function(name, attributes){
-		this.push({event:"open", name: name, attributes: attributes});
-	},
-	"onclosetag", function(name){
-		this.push({event:"close", name: name});
-	},
-	"ontext", function(text){
-		this.push({event:"text", text: text});
-	}
-];
 exports.html = "<h1 class=test>adsf</h1>";
 exports.expected = [ { event: 'open',
     name: 'h1',
