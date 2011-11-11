0013a8710901c72a12b18f6542a72fbd5ee704c7
Felix Böhm <me@feedic.com>
2011-11-11 18:24:47 +0100

    Renamed RssHandler to FeedHandler
    The new name reflects better what it does (it's not just limited to RSS)

--- tests/02-feed.js
+++ tests/02-feed.js
@@ -1,12 +1,12 @@
 //Runs tests for feeds
 
 var helper = require("./test-helper.js"),
-	RssHandler = require("../lib/RssHandler.js");
+	FeedHandler = require("../lib/FeedHandler.js");
 
 exports.dir = "./Feeds/";
 
 exports.test = function(test, cb){
-	var handler = new RssHandler(function(err, dom){
+	var handler = new FeedHandler(function(err, dom){
 		if(err) cb(err, 0); //return the error
 		else cb(null, dom);
 	}, test.options.handler);
