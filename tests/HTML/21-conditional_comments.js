aa272de4402568b618c5659747626f34f61d999d
Felix Böhm <me@feedic.com>
2011-12-09 09:46:44 +0100

    Added oncommentend event, fixed #2
    + added test

---
+++ tests/HTML/21-conditional_comments.js
@@ -0,0 +1,19 @@
+exports.name = "Conditional comments";
+exports.options = {
+    handler: {},
+    parser: {}
+};
+exports.html =
+    "<!--[if lt IE 7]> <html class='no-js ie6 oldie' lang='en'> <![endif]--><!--[if lt IE 7]> <html class='no-js ie6 oldie' lang='en'> <![endif]-->";
+exports.expected = [
+    {
+        data:
+            "[if lt IE 7]> <html class='no-js ie6 oldie' lang='en'> <![endif]",
+        type: "comment"
+    },
+    {
+        data:
+            "[if lt IE 7]> <html class='no-js ie6 oldie' lang='en'> <![endif]",
+        type: "comment"
+    }
+];
