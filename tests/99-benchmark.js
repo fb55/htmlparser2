280a9acf3e473998c700645789f24d922c35da57
Felix Böhm <me@feedic.com>
2011-12-21 17:12:42 +0100

    Some fixes & improvements

--- tests/99-benchmark.js
+++ tests/99-benchmark.js
@@ -1,5 +1,12 @@
 var xml = Array(5e3).join("<!directive><tag attr='value'> text <!--Comment<>--></tag>"),
-	parser = new (require("../lib/Parser.js"))({}),
+	empty = function(){},
+	parser = new (require("../lib/Parser.js"))({
+		onopentag: empty,
+		onclosetag: empty,
+		oncomment: empty,
+		oncommentend: empty,
+		onprocessinginstruction: empty
+	}),
 	ben = require("ben");
 
 console.log("Test took (ms)", ben(1e2, function(){
