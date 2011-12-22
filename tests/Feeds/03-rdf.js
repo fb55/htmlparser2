2eb3243f8acfa08e8d44cc95bbab356365c95e1d
Felix Böhm <me@feedic.com>
2011-12-22 19:55:14 +0100

    Added support for CDATA, optimized regexps

--- tests/Feeds/03-rdf.js
+++ tests/Feeds/03-rdf.js
@@ -1,5 +1,4 @@
 exports.name = "RDF test";
-exports.type = "rss";
 exports.options = {
     handler: {},
     parser: {
@@ -17,17 +16,17 @@ exports.expected = {
     link: "http://sfbay.craigslist.org/ccc/",
     items: [
         {
-            title: "![CDATA[ Music Equipment Repair and Consignment ]]",
+            title: " Music Equipment Repair and Consignment ",
             link: "\nhttp://sfbay.craigslist.org/sby/muc/2681301534.html\n",
             description:
-                "![CDATA[\nSan Jose Rock Shop offers musical instrument repair and consignment! (408) 215-2065"
+                "\nSan Jose Rock Shop offers musical instrument repair and consignment! (408) 215-2065"
         },
         {
             title:
-                "![CDATA[\nRide Offered - Oakland/BART to LA/SFV - TODAY 3PM 11/04 (oakland north / temescal)\n]]",
+                "\nRide Offered - Oakland/BART to LA/SFV - TODAY 3PM 11/04 (oakland north / temescal)\n",
             link: "\nhttp://sfbay.craigslist.org/eby/rid/2685010755.html\n",
             description:
-                "![CDATA[\nIm offering a lift for up to two people from Oakland (or near any BART station in the East Bay/580/880 Corridor, or San Jose/Morgan Hill, Gilroy) to the San Fernando Valley / Los Angeles area. Specifically, Im leaving from Oakland between 2:30 and 3:00pm (this is flexible, but if I leave too late my girlfriend will kill me), and heading to Woodland Hills via the 580, I-5, 405, and 101."
+                "\nIm offering a lift for up to two people from Oakland (or near any BART station in the East Bay/580/880 Corridor, or San Jose/Morgan Hill, Gilroy) to the San Fernando Valley / Los Angeles area. Specifically, Im leaving from Oakland between 2:30 and 3:00pm (this is flexible, but if I leave too late my girlfriend will kill me), and heading to Woodland Hills via the 580, I-5, 405, and 101."
         }
     ]
 };
