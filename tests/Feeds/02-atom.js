3ea9838e43abf3daaedbc292669dac26e821e74a
Felix Böhm <me@feedic.com>
2012-01-06 13:44:49 +0100

    Moved feeds to /tests/Documents
    They shouldn't be inside the test files

--- tests/Feeds/02-atom.js
+++ tests/Feeds/02-atom.js
@@ -6,33 +6,7 @@ exports.options = {
 	}
 };
 exports.type = "rss";
-//http://en.wikipedia.org/wiki/Atom_%28standard%29
-exports.html = '<?xml version="1.0" encoding="utf-8"?>\
-\
-<feed xmlns="http://www.w3.org/2005/Atom">\
-\
-	<title>Example Feed</title>\
-	<subtitle>A subtitle.</subtitle>\
-	<link href="http://example.org/feed/" rel="self" />\
-	<link href="http://example.org/" />\
-	<id>urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6</id>\
-	<updated>2003-12-13T18:30:02Z</updated>\
-	<author>\
-		<name>John Doe</name>\
-		<email>johndoe@example.com</email>\
-	</author>\
-\
-	<entry>\
-		<title>Atom-Powered Robots Run Amok</title>\
-		<link href="http://example.org/2003/12/13/atom03" />\
-		<link rel="alternate" type="text/html" href="http://example.org/2003/12/13/atom03.html"/>\
-		<link rel="edit" href="http://example.org/2003/12/13/atom03/edit"/>\
-		<id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>\
-		<updated>2003-12-13T18:30:02Z</updated>\
-		<summary>Some text.</summary>\
-	</entry>\
-\
-</feed>';
+exports.html = require("fs").readFileSync(__dirname+"/../Documents/Atom_Example.xml").toString();
 exports.expected = {
 	type: "atom",
 	id: "urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6",
