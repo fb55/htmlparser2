9d5e44ffa71d645cda342ed2776b5a87ed0250eb
Felix Böhm <me@feedic.com>
2013-02-15 10:48:47 +0100

    added semicolons & use EE#on in 02-stream.js

--- tests/02-stream.js
+++ tests/02-stream.js
@@ -49,12 +49,12 @@ exports.test = function(test, cb) {
             }
         };
         helper.EVENTS.forEach(function(name) {
-            stream._events[name] = function() {
+            stream.on(name, function() {
                 tokens.push({
                     event: name,
                     data: sliceArr.apply(arguments)
                 });
-            };
+            });
         });
     }
     fs.createReadStream(__dirname + test.file).pipe(stream);
