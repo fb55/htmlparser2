7106b1a901271901630ff1862705bbb2d8a2e8f2
Felix Böhm <me@feedic.com>
2013-04-03 20:17:49 +0200

    [tests] use the new CollectingHandler

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,22 +1,46 @@
 var htmlparser = require(".."),
     Parser = htmlparser.Parser,
+    CollectingHandler = htmlparser.CollectingHandler,
     chunkSize = 5;
 
 exports.writeToParser = function(handler, options, data) {
     var parser = new Parser(handler, options);
     //first, try to run the test via chunks
     for (var i = 0; i < data.length; i += chunkSize) {
-        parser.write(data.substring(i, i + chunkSize));
+        parser.write(data.substr(i, chunkSize));
     }
-    parser.done();
+    parser.end();
     //then parse everything
     parser.parseComplete(data);
 };
 
-var EVENTS = Object.keys(htmlparser.EVENTS);
+//returns a tree structure
+exports.getEventCollector = function(cb) {
+    var handler = new CollectingHandler({
+        onerror: cb,
+        onend: function() {
+            cb(
+                null,
+                handler.events.reduce(function(events, arr) {
+                    if (arr[0] === "onerror" || arr[0] === "onend");
+                    else if (
+                        arr[0] === "ontext" &&
+                        events.length &&
+                        events[events.length - 1].event === "text"
+                    ) {
+                        events[events.length - 1].data[0] += arr[1];
+                    } else {
+                        events.push({
+                            event: arr[0].slice(2),
+                            data: arr.slice(1)
+                        });
+                    }
 
-//remove onend and onerror from events
-EVENTS.splice(EVENTS.indexOf("end"), 1);
-EVENTS.splice(EVENTS.indexOf("error"), 1);
+                    return events;
+                }, [])
+            );
+        }
+    });
 
-exports.EVENTS = EVENTS;
+    return handler;
+};
