fcd7624a7d60d79b35a4f81ecf139fe042981ca3
Felix Böhm <me@feedic.com>
2013-08-26 18:49:20 +0200

    [tests] simplifications

--- test/03-feed.js
+++ test/03-feed.js
@@ -3,20 +3,17 @@
 var helper = require("./test-helper.js"),
     FeedHandler = require("../lib/FeedHandler.js"),
     fs = require("fs"),
-    path = require("path"),
-    parserOpts = {
-        xmlMode: true
-    };
+    path = require("path");
 
 helper.mochaTest("Feeds", __dirname, function(test, cb) {
-    var file = fs.readFile(
-        path.join(__dirname, "Documents", test.file),
-        function(err, file) {
-            helper.writeToParser(
-                new FeedHandler(cb),
-                parserOpts,
-                file.toString()
-            );
-        }
-    );
+    fs.readFile(path.join(__dirname, "Documents", test.file), function(
+        err,
+        file
+    ) {
+        helper.writeToParser(
+            new FeedHandler(cb),
+            { xmlMode: true },
+            file.toString()
+        );
+    });
 });
