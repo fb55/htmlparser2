75d72db08d945f084fd0c9f81e755f3366618d38
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2016-04-01 20:34:17 +0200

    Use StringDecoder for Buffers in WritableStream
    http://stackoverflow.com/questions/12121775/convert-streamed-buffers-to-utf8-string

---
+++ test/unicode.js
@@ -0,0 +1,20 @@
+var htmlparser2 = require(".."),
+    assert = require("assert");
+
+describe("WritableStream", function() {
+    it("should decode fragmented unicode characters", function() {
+        var processed = false;
+        var stream = new htmlparser2.WritableStream({
+            ontext: function(text) {
+                assert.equal(text, "€");
+                processed = true;
+            }
+        });
+
+        stream.write(new Buffer([0xe2, 0x82]));
+        stream.write(new Buffer([0xac]));
+        stream.end();
+
+        assert(processed);
+    });
+});
