da67eba77cbb938e489373181ff40245d422d1c7
Felix Böhm <188768+fb55@users.noreply.github.com>
2021-08-23 13:48:35 +0100

    chore(tests): Move `stream` tests into `WritableStream.spec` (#916)

--- src/WritableStream.spec.ts
+++ src/WritableStream.spec.ts
@@ -1,4 +1,7 @@
+import fs from "fs";
+import path from "path";
 import { WritableStream } from "./WritableStream";
+import * as helper from "./__fixtures__/test-helper";
 
 describe("WritableStream", () => {
     test("should decode fragmented unicode characters", () => {
@@ -12,4 +15,32 @@ describe("WritableStream", () => {
 
         expect(ontext).toBeCalledWith("€");
     });
+});
+
+helper.createSuite("Stream", (test, cb) => {
+    const filePath = path.join(
+        __dirname,
+        "__fixtures__",
+        "Documents",
+        test.file
+    );
+
+    fs.createReadStream(filePath)
+        .pipe(
+            new WritableStream(
+                helper.getEventCollector((err, events) => {
+                    cb(err, events);
+
+                    const handler = helper.getEventCollector(cb);
+                    const stream = new WritableStream(handler, test.options);
+
+                    fs.readFile(filePath, (err, data) => {
+                        if (err) throw err;
+                        stream.end(data);
+                    });
+                }),
+                test.options
+            )
+        )
+        .on("error", cb);
 });
